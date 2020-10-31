f8a823ed012ea1338261cf565dae2874cce322c6
Eden Rose Wallace <wallac33@NSUOK.EDU>
2020-10-31 15:27:31 -0500

    Add project files.

---
+++ Color Switch Game/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Color_Switch_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.block2 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.block1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.scoreList = new System.Windows.Forms.ListBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.block2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.block1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.block1);
+            this.panel1.Controls.Add(this.player);
+            this.panel1.Controls.Add(this.block2);
+            this.panel1.Location = new System.Drawing.Point(0, -6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(481, 550);
+            this.panel1.TabIndex = 0;
+            // 
+            // block2
+            // 
+            this.block2.BackColor = System.Drawing.Color.Red;
+            this.block2.Location = new System.Drawing.Point(-12, 36);
+            this.block2.Name = "block2";
+            this.block2.Size = new System.Drawing.Size(518, 30);
+            this.block2.TabIndex = 0;
+            this.block2.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Yellow;
+            this.player.Location = new System.Drawing.Point(191, 502);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(90, 40);
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            // 
+            // block1
+            // 
+            this.block1.BackColor = System.Drawing.Color.Red;
+            this.block1.Location = new System.Drawing.Point(-12, 240);
+            this.block1.Name = "block1";
+            this.block1.Size = new System.Drawing.Size(589, 25);
+            this.block1.TabIndex = 2;
+            this.block1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(488, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Score: 0 ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(541, 496);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Press Space to Change Color\r\n    Press R to Reset Game";
+            this.label2.UseMnemonic = false;
+            // 
+            // scoreList
+            // 
+            this.scoreList.Enabled = false;
+            this.scoreList.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreList.FormattingEnabled = true;
+            this.scoreList.ItemHeight = 18;
+            this.scoreList.Location = new System.Drawing.Point(493, 80);
+            this.scoreList.Name = "scoreList";
+            this.scoreList.Size = new System.Drawing.Size(251, 364);
+            this.scoreList.TabIndex = 4;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.playGame);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(648, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Start Playing ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 556);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.scoreList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Color Switch";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyisDown);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.block2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.block1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox block1;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox block2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox scoreList;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Color Switch Game/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Color_Switch_Game
+{
+    
+    public partial class Form1 : Form
+    {
+        List<Color> colors;
+        Random rnd = new Random();
+        Random blockPosition = new Random();
+        int blockColor = 0;
+        int i;
+        int speed = 5;
+        int score = 0;
+        bool gameOver = false;
+
+        public Form1()
+        {
+            this.KeyPreview = true;
+            InitializeComponent();
+            resetGame();
+            gameTimer.Stop();
+
+        }
+
+        private void playGame(object sender, EventArgs e)
+        {
+            block1.Top += speed; 
+            block2.Top += speed; 
+            label1.Text = "Score " + score;
+
+            if (block1.Top > 500)
+            { 
+                blockColor = rnd.Next(colors.Count);
+                block1.BackColor = colors[blockColor]; 
+                block1.Top = blockPosition.Next(200, 300) * -1; 
+                score++; 
+            }
+
+            if (block2.Top > 500)
+            {
+               
+                if (block1.Top > 100)
+                {
+                    blockColor = rnd.Next(colors.Count); 
+                    block2.BackColor = colors[blockColor]; 
+                    block2.Top = (block1.Top * 8) * -1;
+                    score++; 
+                }
+            }
+
+            if (player.Bounds.IntersectsWith(block1.Bounds))
+            {
+                if (player.BackColor != block1.BackColor)
+                {
+                    scoreList.Items.Add("Scored: " + score + " @" + string.Format(" {0:HH:mm:ss tt}", DateTime.Now));
+                    gameTimer.Stop(); 
+                    gameOver = true; 
+                }
+            }
+
+            if (player.Bounds.IntersectsWith(block2.Bounds))
+            {
+                if (player.BackColor != block2.BackColor)
+                {
+                    scoreList.Items.Add("Scored: " + score + " @" + string.Format(" {0:HH:mm:ss tt}", DateTime.Now));
+                    gameTimer.Stop(); 
+                    gameOver = true;
+                }
+            }
+
+            if (score > 5)
+            {
+                speed = 6;
+            }
+
+            if (score > 10)
+            {
+                speed = 8;
+            }
+
+            block1.Refresh();
+            block2.Refresh();
+        }
+
+        private void KeyisDown(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Space)
+            {
+                i++; 
+
+                    if (i > colors.Count - 1)
+                {
+                    i = 0;
+                }
+                player.BackColor = colors[i];
+            }
+            if (e.KeyChar == (char)Keys.R || e.KeyChar == char.ToLower((char)Keys.R) && gameOver)
+            {
+                resetGame();
+                gameOver = false; 
+            }
+        }
+
+        public void resetGame()
+        {
+            block1.Top = -80;
+            block2.Top = -300;
+
+            colors = new List<Color> { System.Drawing.Color.Red, System.Drawing.Color.Yellow, System.Drawing.Color.White, System.Drawing.Color.Purple };
+
+            i = 0;
+            gameTimer.Start();
+            speed = 5;
+            score = 0; 
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+
+         
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            resetGame();
+             
+        }
+    }
+
+
+}
